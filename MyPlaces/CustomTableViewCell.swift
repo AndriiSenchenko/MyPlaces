970654bf9f95d6ce9036a2275744112e47927c86
Andrey <777senchenko777@gmail.com>
2022-08-25 20:13:56 +0300

    Implement custom cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Andrey on 25.08.2022.
+//  Copyright © 2022 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var lacationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+
+
+}
